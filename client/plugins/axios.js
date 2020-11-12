27bc8b2a60c5319d6178d5eb6ffe4dff6f0af74a
spacemonkey <spacemonkey925@gmail.com>
2020-11-12 15:15:29 +0900

    .

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,15 +1,17 @@
 export default (ctx, inject) => {
+  /**
+   * * API 요청시
+   */
   ctx.$axios.onRequest(({ url }) => {
     console.log(`Make request to ${url}`)
   })
-  ctx.$axios.onResponse(({ data }) => {
-    console.log(data.ok)
-    // if (!data.ok) {
-    //   alert('토큰 기간이 만료되었습니다. 다시 로그인해주세요.')
-    //   ctx.store.dispatch('user/logout')
-    //   ctx.redirect({ name: 'user-login' })
-    // }
-  })
+  /**
+   * * API 응답시
+   */
+  ctx.$axios.onResponse(({ data }) => {})
+  /**
+   * * API 에러시
+   */
   ctx.$axios.onError((e) => {
     const { response } = e
     if (!response.data.ok && response.data.errorState.code === 401) {
